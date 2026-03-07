214a3270e42d5dfacfed39d067130020da6a11fb
uitgeteld <66201378+uitgeteld@users.noreply.github.com>
2026-03-07 16:46:51 +0100

    Initial commit

---
+++ 1.21.1/create-better-factory/src/main/java/xyz/uitgeteld/cbf/Cbf.java
@@ -0,0 +1,11 @@
+package xyz.uitgeteld.cbf;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+
+@Mod(Cbf.MODID)
+public class Cbf {
+    public static final String MODID = "cbf";
+    public Cbf(IEventBus modEventBus) {
+    }
+}
